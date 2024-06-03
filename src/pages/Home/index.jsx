5fca6ce472947cf25d172ad5d8ad013612f1b8e8
Heriwill <willian.herisson@gmail.com>
2024-06-03 17:28:39 -0300

    Adição de imagem e carde

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,27 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import Image from 'react-bootstrap/Image';
+
 import './home.css'
 
 function Home(){
 
     return(
       <>  
-        <div>
+        
            <img src="../images/imgfundo.jpeg" alt="" />
-          <div className='texto1'>teste pq sim</div>
-           <h1 className='titulo'>Projeto Academia</h1>
-           <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, ratione soluta, nobis alias sint modi, ipsam accusamus fugit sit ex consequatur. Doloremque voluptatem omnis, eum quasi facilis porro quisquam totam?</h2>
+
+           
+           <h1 id='titulo'>PowerFlex Academy
+          <p className='texto1'>diferenciar a o tamnho de letra do título do conteudo</p> 
+          <p className='texto1'>ascaaaaaaaaaaaaaaaaaa ascccccccccccccccccccccccc sacccccccccccccccccCuidado, o texto passa do espaço sjsjs sjsjs sjsjs jsjsjssssssssssssssssssssssssssssssssssss</p>
+           </h1>
+
+           <h2 >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, ratione soluta, nobis alias sint modi, ipsam accusamus fugit sit ex consequatur. Doloremque voluptatem omnis, eum quasi facilis porro quisquam totam?</h2>
 
             <p></p>
             <br /><br />
-        </div>
+        
         <p></p><br />
 
-        <div  className='cardetextoo'>
-        <Card border="primary" style={{ width: '18rem' }}>
+
+        <Card border="primary" style={{ width: '18rem' }} >
         <Card.Header>Header</Card.Header>
-        <Card.Body>
+        <Card.Body className='cardecorpo'>
           <Card.Title>Primary Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
@@ -30,17 +37,59 @@ function Home(){
           </Card.Text>
         </Card.Body>
       </Card>
-      </div>
+    
       <br />
 
+      <img src="../images/imgcarde2.jpeg" id='imgmeio'/>
+
+      <Card border="primary" className='cardedireita' >
+        <Card.Header>Header, mudar para esquerda da margem</Card.Header>
+        <Card.Body className='cardecorpo'>
+          <Card.Title>Primary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    
+    <br />
+
+    <Card border="primary" className='cardedireita' >
+        <Card.Header>Header</Card.Header>
+        <Card.Body className='cardecorpo'>
+          <Card.Title>Primary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+    <br />
+
+    <Card border="primary" style={{ width: '18rem' }} >
+        <Card.Header>Header</Card.Header>
+        <Card.Body className='cardecorpo'>
+          <Card.Title>Primary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+<   br />
 
         <div>
         <CardGroup>
       <Card>
-        <Card.Img variant="top" src="../images/imgcarde1.jpeg" />
-        <Card.Body className='corpocarde'>
-          <Card.Title className='cardetitulo'>XXXXXXXX</Card.Title>
-          <Card.Text>
+        <Card.Img className='Card.Img' variant="top" src="../images/imgcarde1.jpeg" />
+        <Card.Body className='cardecorpo'>
+          <Card.Title className='cardetitulo'>
+          <a href="http://" className='link'>XXXXXXXXXXXXXXX</a>  
+          </Card.Title>
+          <Card.Text className='cardetexto'>
             This is a wider card with supporting text below as a natural lead-in
             to additional content. This content is a little bit longer.
           </Card.Text>
@@ -50,10 +99,12 @@ function Home(){
         </Card.Footer>
       </Card>
       <Card>
-        <Card.Img variant="top" src="../images/imgcarde2.jpeg" />
-        <Card.Body>
-          <Card.Title>cadastre-se</Card.Title>
-          <Card.Text>
+        <Card.Img variant="top" src="../images/imgcarde2.jpeg" heref=""/>
+        <Card.Body className='cardecorpo'>
+          <Card.Title className='cardetitulo'>
+          <a href="../Usuario" className='link'>cadastre-se</a>  
+          </Card.Title>
+          <Card.Text className='cardetexto'>
             This card has supporting text below as a natural lead-in to
             additional content.{' '}
           </Card.Text>
@@ -64,9 +115,11 @@ function Home(){
       </Card>
       <Card>
         <Card.Img variant="top" src="../images/imgcarde1.jpeg" />
-        <Card.Body>
-          <Card.Title>Acesse o App</Card.Title>
-          <Card.Text>
+        <Card.Body className='cardecorpo'>
+          <Card.Title className='cardetitulo'>
+          <a href="http://" className='link'>Acesse o App</a>
+          </Card.Title>
+          <Card.Text className='cardetexto'>
             This is a wider card with supporting text below as a natural lead-in
             to additional content. This card has even longer content than the
             first to show that equal height action.
@@ -80,7 +133,10 @@ function Home(){
           </div>
     
 
-
+      <footer>
+        <p>Footer</p>
+        <p>Informacoes finais</p>
+      </footer>
      
 
     </>   
