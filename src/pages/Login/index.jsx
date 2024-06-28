2c4cdc1e3bd9d0e125c36a1b21a1788ac4caa907
Heriwill <willian.herisson@gmail.com>
2024-06-27 22:18:26 -0300

    Projeto OK

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -9,7 +9,12 @@ const Login = () => {
 
     const onSubmit = (data) => {
         // Impede que a página seja recarregada
-        //data.preventDefault();
+        //preventDefault();
+          try {
+            if (!register) {
+              alert("Por favor, preencha todos os campos!");
+              return;
+            }
         alert(JSON.stringify(data)) 
        
         //if (data.value.trim() === '') {
@@ -19,6 +24,10 @@ const Login = () => {
     
       // Faz o console log das credenciais do usuário
       console.log("Dados de Login:", { data });
+      
+          }catch (error) {
+            console.log(error);
+          }
     };
 
   return (
@@ -51,6 +60,67 @@ const Login = () => {
 
 
 export default Login;
+
+
+
+
+/*
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
+import { FaUser, FaLock } from "react-icons/fa";
+import './login.css';
+
+const Login = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    console.log("Dados de Login:", { data });
+  };
+
+  return (
+    <div className="app-container">
+      <div className="form-group">
+        <label>E-mail</label>
+        <input
+          type="email"
+          placeholder="Seu Email"
+          maxLength={40}
+          {...register("email", { required: true })}
+        />
+        {errors.email && <span>Campo obrigatório</span>}
+        <FaUser className="icon" />
+      </div>
+
+      <div className="form-group">
+        <label>Senha</label>
+        <input
+          type="password"
+          placeholder="Sua Senha"
+          maxLength={10}
+          {...register("senha", { required: true })}
+        />
+        {errors.senha && <span>Campo obrigatório</span>}
+        <FaLock className="icon" />
+      </div>
+
+      <div className="form-group">
+        <button onClick={handleSubmit(onSubmit)}>Entrar</button>
+        <p>
+          Não tem uma conta? <a href="../usuario">Cadastre-se</a>{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+*/
+/*
+
+
+/*
+
 /*
 import React from 'react';
 import { Link } from 'react-router-dom';
